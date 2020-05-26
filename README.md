ce8793d80aa97e5bff3af3ef3598a397b19ab1fa
Jazzest <josephwhacheng98@gmail.com>
2020-05-25 19:29:19 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# MVC NodeJS Aplication - Only Create and Read
+
+### Database agenciaviajes (included):
+- viajes table
+  - rows: id, titulo, precio, fecha_ida, fecha_vuelta, imagen, descripcion, disponibles
+- testimoniales table
+  - rows: id, nombre, correo, mensaje
+
+### Dependencies:
+- [Pugjs](https://pugjs.org/api/getting-started.html)
+- [Body Parser](https://expressjs.com/en/resources/middleware/body-parser.html)
+- [Express](https://expressjs.com/)
+- [Nodemon](https://nodemon.io/)
+- [Sequalize](https://sequelize.org/)
+- [NodeJS](https://nodejs.org/)
+
+### Run the application
+```
+  npm start
+```
+
---
+++ server/index.js
@@ -0,0 +1,47 @@
+// Importar Express
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const configs = require('./config');
+const db = require('./config/database');
+
+db.authenticate()
+  .then(() => console.log('DB conectada'))
+  .catch((error) => console.log('error'));
+
+// Configurar Express
+const app = express();
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Agregar las vistas
+app.set('views', path.join(__dirname, './views'));
+
+// Cargar la carpeta estatica - public
+app.use(express.static('public'));
+
+// Validar si estamos en dev o prod
+const config = configs[app.get('env')];
+
+// Creamos la variable para el sitio web
+app.locals.titulo = config.nombresitio;
+
+// Crear variables locales para los templates
+app.use((req, res, next) => {
+  // Crear una nueva fecha
+  const fecha = new Date();
+  res.locals.fechaActual = fecha.getFullYear();
+  res.locals.ruta = req.path;
+  return next();
+});
+
+// Ejecutamos el body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cargar las rutas
+app.use('/', routes());
+
+app.listen(5000);
